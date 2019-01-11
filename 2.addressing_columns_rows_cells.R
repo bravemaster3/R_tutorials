dac7060ce048aad4a98f4379870d9bbf99c915b8
bravemaster3 <39010414+bravemaster3@users.noreply.github.com>
2019-01-11 19:46:58 +0100

    Update 2.addressing_columns_rows_cells.R

--- 2.addressing_columns_rows_cells.R
+++ 2.addressing_columns_rows_cells.R
@@ -6,4 +6,23 @@
 #2: Check the data type of the "time" column
 
 
-# If the time column is not a POSIXct object, then convert it to 
+#3: If the time column is not a POSIXct object, then convert it
+
+
+#Addressing a column of the dataframe
+air_temp2[,"year"] #OR
+air_temp2$year #both the previous and this line mean all rows, column year
+    #You can do the same by replacing "year" by its numer (1 is the first column, time in our case) in the dataframe columns order
+#Addressing a row of the dataframe
+air_temp2[1,] #This means first row, all columns # If rows had names, 1 could be replaced by the name of the first row. In our case rows don't have names
+
+#From what we've seen previously, addressing rows and columns, we could guess that addressing a cell would be as follows
+air_temp2[1,2] #or
+air_temp2[1,"Tair"] #means cell at first row and column "Tair"
+
+#subsetting our dataframe
+air_temp_2016 <- air_temp2[which(air_temp2$year==2016),] # would return all columns and rows where the column year=2016
+
+#Based on our previous exercise (1.1.reading_file_exploring_editing_writing.R), write air_temp_2016 to a new file on disk, with the name air_temp_sub_2016
+
+
