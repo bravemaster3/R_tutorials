b9ba684efc141ba34b752918bc6642a0e52049eb
bravemaster3 <39010414+bravemaster3@users.noreply.github.com>
2019-01-11 18:57:13 +0100

    Update 2.addressing_columns_rows_cells.R

--- 2.addressing_columns_rows_cells.R
+++ 2.addressing_columns_rows_cells.R
@@ -1,1 +1,9 @@
+# Previously, we learnt how to read a timeseries of air temperature, explore it and write the edited version to a file.
+# Complete this file before:
+#1: Read air_temp_edited from disk to a variable named air_temp2
 
+
+#2: Check the data type of the "time" column
+
+
+# If the time column is not a POSIXct object, then convert it to 
