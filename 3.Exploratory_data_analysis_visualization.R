4924d8f94ac61279b25139e7f0612808037bea3f
bravemaster3 <39010414+bravemaster3@users.noreply.github.com>
2019-01-11 20:20:18 +0100

    Create 3.Exploratory_data_analysis_visualization.R

---
+++ 3.Exploratory_data_analysis_visualization.R
@@ -0,0 +1,7 @@
+# An important step in statistics or data analysis in general is the visualization of data.
+# Import air_temp_edited to a variable you will name air_temp (output of section 1.reading_file_exploring_editing_writing.R) and do all the necessary (i.e convert factor to time object if necessary)
+
+#Let's make our first graph
+plot(Tair~time, data=air_temp) #pretty easy right?
+
+#
