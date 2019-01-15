609ac08f4a3ebb74679c1fcc25cbe76d56d5e2a9
bravemaster3 <39010414+bravemaster3@users.noreply.github.com>
2019-01-15 14:08:50 +0100

    Update 3.Exploratory_data_analysis_visualization.R

--- 3.Exploratory_data_analysis_visualization.R
+++ 3.Exploratory_data_analysis_visualization.R
@@ -3,5 +3,11 @@
 
 #Let's make our first graph
 plot(Tair~time, data=air_temp) #pretty easy right?
+#Let's plot again, specifying the title of the X and Y axis (xlab and ylab)
+plot(Tair~time, data=air_temp, xlab="Time", ylab="Air Temperature")
+#Changing the color (col), symbol(pch) and size of the points
+plot(Tair~time, data=air_temp, xlab="Time", ylab="Air Temperature",col="red")
+plot(Tair~time, data=air_temp, xlab="Time", ylab="Air Temperature",col="red", pch=16)
+plot(Tair~time, data=air_temp, xlab="Time", ylab="Air Temperature",col="red", pch=16, cex=0.02)
 
-#
+
