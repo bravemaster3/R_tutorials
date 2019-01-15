11227df78b42105e8e6482b9b9962d34618a393b
ericacarnea <46590739+ericacarnea@users.noreply.github.com>
2019-01-15 13:50:45 +0100

    Update 2.addressing_columns_rows_cells.R

--- 2.addressing_columns_rows_cells.R
+++ 2.addressing_columns_rows_cells.R
@@ -1,34 +1,53 @@
 # Previously, we learnt how to read a timeseries of air temperature, explore it and write the edited version to a file.
 # Complete this file before:
 #1: Read air_temp_edited from disk to a variable named air_temp2
-
+air_temp2 <- read.table("G:/EASY_R/air_temp_edited.csv", header=TRUE, sep=",")
 
 #2: Check the data type of the "time" column
-
+str(air_temp2$time)
 
 #3: If the time column is not a POSIXct object, then convert it
-
+air_temp2$time<-as.POSIXct(air_temp2$time, "%Y-%m-%d %H:%M:%S", tz="")
 
 #Addressing a column of the dataframe
 air_temp2[,"year"] #OR
 air_temp2$year #both the previous and this line mean all rows, column year
-    #You can do the same by replacing "year" by its numer (1 is the first column, time in our case) in the dataframe columns order
+#You can do the same by replacing "year" by its numer (1 is the first column, time in our case) in the dataframe columns order
 #Addressing a row of the dataframe
 air_temp2[1,] #This means first row, all columns # If rows had names, 1 could be replaced by the name of the first row. In our case rows don't have names
 
 #From what we've seen previously, addressing rows and columns, we could guess that addressing a cell would be as follows
 air_temp2[1,2] #or
 air_temp2[1,"Tair"] #means cell at first row and column "Tair"
+x<-3
+
+#Izpiši vse vrednosti, ki so pred datumom 2015-01-01 
+which(air_temp2$time < as.POSIXct('2015-01-01 00:00:00', format ='%Y-%m-%d %H:%M:%S', tz=''))
 
 #subsetting our dataframe getting some rows only based on values of a column
-air_temp_bef_2015 <- air_temp2[which(air_temp$time < as.POSIXct('2015-01-01 00:00:00', format ='%Y-%m-%d %H:%M:%S', tz='')),] #would return all rows before 2015
+air_temp_bef_2015 <- air_temp2[which(air_temp2$time < as.POSIXct('2015-01-01 00:00:00', format ='%Y-%m-%d %H:%M:%S', tz='')),] #would return all rows before 2015
+
 air_temp_2016 <- air_temp2[which(air_temp2$year==2016),] # would return all columns and rows where the column year=2016
 
-#Based on our previous exercise (1.1.reading_file_exploring_editing_writing.R), write air_temp_2016 to a new file on disk, with the name air_temp_sub_row_2016
 
+
+#Based on our previous exercise (1.1.reading_file_exploring_editing_writing.R), write air_temp_2016 to a new file on disk, with the name air_temp_sub_row_2016
+write.table(air_temp_2016,"G:/EASY_R/air_temp_sub_row_2016.csv", sep=",", 
+            append = FALSE, quote = TRUE, 
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = TRUE, qmethod = c("escape", "double"))
 
 #Similarly, we can reduce our dataframe to only get some columns with all the rows 
-air_temp_some_cols <- air_temp2[, c(1:3, "hour")]
+#
+
+v<-c(1,6,7,10)
+v2<-c("Gregor","Erika","Koffi")
+length(v2)
+length(air_temp2$time [which(air_temp2$year==2015)])
+#izpiši vse vrednosti v tabeli air_temp2 in sicer vse vrstice v stolpcih 1, 2 in 3
+air_temp2[c(1,4,5), c(1:3)]
+
+air_temp_some_cols <- air_temp2[, c("time","hour")]
 
 #######
 #BONUS#
@@ -37,5 +56,5 @@ air_temp_some_cols <- air_temp2[, c(1:3, "hour")]
 #Or also formats like '%Y%d%m %H.%M.%S' (this was an example, and it needs to be checked)... We cannot apply this to the column in the csv file we read from disk, because it is not in that format.
 #But it can be tested by converting an object string, to POSIXct
 #eg.
-time_x <- "20162702 12.20.00"
-time_x_posixct <- as.POSIXct(time_x, format = '%Y%d%m %H.%M.%S', tz ="")
+time_x <- "20162702122000"
+time_x_posixct <- as.POSIXct(time_x, format = '%Y%d%m%H%M%S', tz ="")
